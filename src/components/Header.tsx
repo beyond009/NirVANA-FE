85aa53f603c2ca67433bbcd9d920bbabc9c347d9
byeond009 <1249413181@qq.com>
2023-04-05 15:27:35 +0800

    feat: add flowbite & button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ConnectWallet from '@/components/ConnectWallet'
-// import { Logo } from './Logo'
+import Image from 'next/image'
+
 export const Header = () => {
 	return (
 		<div className="flex justify-between w-full max-w-6xl mx-auto py-12 bg-gray-100 dark:bg-gray-900 sm:px-6 lg:px-8">
-			<div className="flex items-center text-4xl m-[-22px]">Name</div>
+			<div className="flex items-center text-3xl m-[-22px] gap-3 font-serif">
+				<Image src="/Nirvana_Face.svg" width={40} height={40} priority alt="logo" /> NirVANA
+			</div>
 			<div className="flex">
 				<ConnectWallet />
 			</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
-	content: ['./src/**/*.{js,ts,jsx,tsx}'],
+	content: ['./src/**/*.{js,ts,jsx,tsx}', 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
 	darkMode: 'class',
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('flowbite/plugin')],
 }
