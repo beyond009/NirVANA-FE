5501ff0fecc5508b0f7a03d242714f7e6db51814
byeond009 <1249413181@qq.com>
2023-04-06 10:57:26 +0800

    feat: add router

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,58 @@
 import React from 'react'
 import ConnectWallet from '@/components/ConnectWallet'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export const Header = () => {
+	const router = useRouter()
 	return (
-		<div className="flex justify-between w-full max-w-6xl mx-auto py-12 bg-gray-100 dark:bg-gray-900 sm:px-6 lg:px-8">
+		<div className="relative flex justify-between w-full max-w-6xl mx-auto py-12 bg-gray-100 dark:bg-gray-900 sm:px-6 lg:px-8">
 			<div className="flex items-center text-3xl m-[-22px] gap-3 font-serif">
 				<Image src="/Nirvana_Face.svg" width={40} height={40} priority alt="logo" /> NirVANA
 			</div>
-			<div className="flex">
-				<ConnectWallet />
+			<div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+				<ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border md:flex-row md:space-x-8 md:mt-0 md:border-0">
+					<li>
+						<a
+							className={`block py-2 pl-3 pr-4 cursor-pointe rounded hover:text-blue-500 ${
+								router.pathname === '/' ? 'text-blue-700' : ''
+							}`}
+							aria-current="page"
+							onClick={() => {
+								router.push('/')
+							}}
+						>
+							Home
+						</a>
+					</li>
+					<li>
+						<a
+							className={`block py-2 pl-3 pr-4 cursor-pointer rounded  hover:text-blue-500 ${
+								router.pathname === '/create-sbt' ? 'text-blue-700' : ''
+							}`}
+							onClick={() => {
+								router.push('/create-sbt')
+							}}
+						>
+							create sbt
+						</a>
+					</li>
+					<li>
+						<a
+							className={`block py-2 pl-3 pr-4 cursor-pointer rounded hover:text-blue-500 ${
+								router.pathname === '/create-plugin' ? 'text-blue-700' : ''
+							}`}
+							onClick={() => {
+								router.push('/create-plugin')
+							}}
+						>
+							create plugin
+						</a>
+					</li>
+				</ul>
+				<div className="flex ml-6">
+					<ConnectWallet />
+				</div>
 			</div>
 		</div>
 	)
