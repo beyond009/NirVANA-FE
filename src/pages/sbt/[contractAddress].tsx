df4f5e240dc5d3cb158ffa70bad61b1a78241e78
byeond009 <1249413181@qq.com>
2023-04-08 01:25:52 +0800

    feat: update sbt page

--- src/pages/sbt/[contractAddress].tsx
+++ src/pages/sbt/[contractAddress].tsx
@@ -10,11 +10,22 @@ export const SBT = () => {
 			<div className="text-xl">{contractAddress}</div>
 			<div className="text-2xl">SBT Plugins</div>
 			<div className="grid grid-cols-4 gap-6 py-6">
-				<a className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+				<a className="block max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 						Simple governace
 					</h5>
-					<p className="font-normal text-gray-700 dark:text-gray-400">{v.desc}</p>
+					<p className="font-normal text-gray-700 dark:text-gray-400">
+						a simple DAO with proposal creation, voting, and execution functionality to issue sbt.
+					</p>
+				</a>
+				<a className="block max-w-2xl p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+						Signature Recovery
+					</h5>
+					<p className="font-normal text-gray-700 dark:text-gray-400">
+						allowing users to recover owned sbts from a signed message,The contract leverages EIP-712 for
+						typed data hashing and ECDSA for signature recovery.
+					</p>
 				</a>
 			</div>
 		</div>
